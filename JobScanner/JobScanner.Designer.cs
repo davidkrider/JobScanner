9d74698a6efc83bf1588295f0ba177b025d2de95
David Krider <david@davidkrider.com>
2016-02-28 00:18:48 -0500

    Initial commit.

---
+++ JobScanner/JobScanner.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace JobScanner
+{
+    partial class FormJobScanner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormJobScanner));
+            this.labelVim = new System.Windows.Forms.Label();
+            this.textBoxVim = new System.Windows.Forms.TextBox();
+            this.textBoxEmployee = new System.Windows.Forms.TextBox();
+            this.labelEmployee = new System.Windows.Forms.Label();
+            this.textBoxLot = new System.Windows.Forms.TextBox();
+            this.labelLot = new System.Windows.Forms.Label();
+            this.textBoxItem = new System.Windows.Forms.TextBox();
+            this.labelItem = new System.Windows.Forms.Label();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.labelQuantity = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelVim
+            // 
+            this.labelVim.AutoSize = true;
+            this.labelVim.Location = new System.Drawing.Point(12, 9);
+            this.labelVim.Name = "labelVim";
+            this.labelVim.Size = new System.Drawing.Size(26, 13);
+            this.labelVim.TabIndex = 2;
+            this.labelVim.Text = "VIM";
+            // 
+            // textBoxVim
+            // 
+            this.textBoxVim.Location = new System.Drawing.Point(91, 6);
+            this.textBoxVim.Name = "textBoxVim";
+            this.textBoxVim.Size = new System.Drawing.Size(181, 20);
+            this.textBoxVim.TabIndex = 3;
+            // 
+            // textBoxEmployee
+            // 
+            this.textBoxEmployee.Location = new System.Drawing.Point(91, 32);
+            this.textBoxEmployee.Name = "textBoxEmployee";
+            this.textBoxEmployee.Size = new System.Drawing.Size(181, 20);
+            this.textBoxEmployee.TabIndex = 5;
+            // 
+            // labelEmployee
+            // 
+            this.labelEmployee.AutoSize = true;
+            this.labelEmployee.Location = new System.Drawing.Point(12, 35);
+            this.labelEmployee.Name = "labelEmployee";
+            this.labelEmployee.Size = new System.Drawing.Size(53, 13);
+            this.labelEmployee.TabIndex = 4;
+            this.labelEmployee.Text = "Employee";
+            // 
+            // textBoxLot
+            // 
+            this.textBoxLot.Location = new System.Drawing.Point(91, 58);
+            this.textBoxLot.Name = "textBoxLot";
+            this.textBoxLot.Size = new System.Drawing.Size(181, 20);
+            this.textBoxLot.TabIndex = 7;
+            // 
+            // labelLot
+            // 
+            this.labelLot.AutoSize = true;
+            this.labelLot.Location = new System.Drawing.Point(12, 61);
+            this.labelLot.Name = "labelLot";
+            this.labelLot.Size = new System.Drawing.Size(22, 13);
+            this.labelLot.TabIndex = 6;
+            this.labelLot.Text = "Lot";
+            // 
+            // textBoxItem
+            // 
+            this.textBoxItem.Location = new System.Drawing.Point(91, 84);
+            this.textBoxItem.Name = "textBoxItem";
+            this.textBoxItem.Size = new System.Drawing.Size(181, 20);
+            this.textBoxItem.TabIndex = 9;
+            // 
+            // labelItem
+            // 
+            this.labelItem.AutoSize = true;
+            this.labelItem.Location = new System.Drawing.Point(12, 87);
+            this.labelItem.Name = "labelItem";
+            this.labelItem.Size = new System.Drawing.Size(27, 13);
+            this.labelItem.TabIndex = 8;
+            this.labelItem.Text = "Item";
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(91, 110);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(181, 20);
+            this.textBoxQuantity.TabIndex = 11;
+            // 
+            // labelQuantity
+            // 
+            this.labelQuantity.AutoSize = true;
+            this.labelQuantity.Location = new System.Drawing.Point(12, 113);
+            this.labelQuantity.Name = "labelQuantity";
+            this.labelQuantity.Size = new System.Drawing.Size(46, 13);
+            this.labelQuantity.TabIndex = 10;
+            this.labelQuantity.Text = "Quantity";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(197, 147);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 12;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // FormJobScanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 182);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.labelQuantity);
+            this.Controls.Add(this.textBoxItem);
+            this.Controls.Add(this.labelItem);
+            this.Controls.Add(this.textBoxLot);
+            this.Controls.Add(this.labelLot);
+            this.Controls.Add(this.textBoxEmployee);
+            this.Controls.Add(this.labelEmployee);
+            this.Controls.Add(this.textBoxVim);
+            this.Controls.Add(this.labelVim);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormJobScanner";
+            this.Text = "Job Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVim;
+        private System.Windows.Forms.TextBox textBoxVim;
+        private System.Windows.Forms.TextBox textBoxEmployee;
+        private System.Windows.Forms.Label labelEmployee;
+        private System.Windows.Forms.TextBox textBoxLot;
+        private System.Windows.Forms.Label labelLot;
+        private System.Windows.Forms.TextBox textBoxItem;
+        private System.Windows.Forms.Label labelItem;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.Label labelQuantity;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ JobScanner/JobScanner.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using CoreScanner;
+using System.Diagnostics;
+using System.Xml;
+using System.Net;
+using System.Text.RegularExpressions;
+
+
+namespace JobScanner
+{
+    public partial class FormJobScanner : Form
+    {
+        CCoreScannerClass m_pCoreScanner;
+
+        // Scanner types
+        public const short SCANNER_TYPES_ALL = 1;
+        public const short SCANNER_TYPES_SNAPI = 2;
+        public const short SCANNER_TYPES_SSI = 3;
+        public const short SCANNER_TYPES_RSM = 4;
+        public const short SCANNER_TYPES_IMAGING = 5;
+        public const short SCANNER_TYPES_IBMHID = 6;
+        public const short SCANNER_TYPES_NIXMODB = 7;
+        public const short SCANNER_TYPES_HIDKB = 8;
+        public const short SCANNER_TYPES_IBMTT = 9;
+        public const short SCALE_TYPES_IBM = 10;
+
+
+        public FormJobScanner()
+        {
+            InitializeComponent();
+
+            try
+            {
+                m_pCoreScanner = new CoreScanner.CCoreScannerClass();
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(1000);
+                m_pCoreScanner = new CoreScanner.CCoreScannerClass();
+            }
+
+            int status = 1;
+            short[] m_arScannerTypes = new short[1];
+            m_arScannerTypes[0] = 1;
+            short m_nNumberOfTypes = 1;
+            m_pCoreScanner.Open(0, m_arScannerTypes, m_nNumberOfTypes, out status);
+
+            m_pCoreScanner.BarcodeEvent += new _ICoreScannerEvents_BarcodeEventEventHandler(OnBarcodeEvent);
+
+            string inXml = "<inArgs>" +
+                            "<cmdArgs>" +
+                            "<arg-int>6</arg-int>" +
+                            "<arg-int>1,2,4,8,16,32</arg-int>" +
+                            "</cmdArgs>" +
+                            "</inArgs>";
+            string outXml = "";
+            status = 1;
+            m_pCoreScanner.ExecCommand(1001, ref inXml, out outXml, out status);
+            Debug.Print(" *** Status: " + status.ToString());
+
+        }
+
+        void OnBarcodeEvent(short eventType, ref string scanData)
+        {
+            try
+            {
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(scanData);
+                string barcode = xmlDoc.DocumentElement.GetElementsByTagName("datalabel").Item(0).InnerText;
+
+                string strData = String.Empty;
+                string[] numbers = barcode.Split(' ');
+                foreach (string number in numbers)
+                {
+                    if (String.IsNullOrEmpty(number))
+                    {
+                        break;
+                    }
+                    strData += ((char)Convert.ToInt32(number, 16)).ToString();
+                }
+
+                if (Regex.IsMatch(strData, @"\d+-\d+"))
+                {
+                    textBoxItem.Invoke(new MethodInvoker(delegate
+                    {
+                        textBoxItem.Clear();
+                        textBoxItem.Text = strData;
+                    }));
+                }
+                else if (Regex.IsMatch(strData, @"\d{12}"))
+                {
+                    textBoxLot.Invoke(new MethodInvoker(delegate
+                    {
+                        textBoxLot.Clear();
+                        textBoxLot.Text = strData;
+                    }));
+                }
+                else
+                {
+                    textBoxQuantity.Invoke(new MethodInvoker(delegate
+                    {
+                        textBoxQuantity.Clear();
+                        textBoxQuantity.Text = strData;
+                    }));
+                }
+
+                if (!String.IsNullOrEmpty(textBoxVim.Text) && !String.IsNullOrEmpty(textBoxEmployee.Text) &&
+                    !String.IsNullOrEmpty(textBoxLot.Text) && !String.IsNullOrEmpty(textBoxQuantity.Text) &&
+                    !String.IsNullOrEmpty(textBoxItem.Text))
+                {
+                    postJob(textBoxVim.Text, textBoxEmployee.Text, textBoxLot.Text,
+                        textBoxItem.Text, textBoxQuantity.Text);
+                    textBoxVim.Clear();
+                    textBoxEmployee.Clear();
+                    textBoxLot.Clear();
+                    textBoxQuantity.Clear();
+                    textBoxItem.Clear();
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+
+            postJob(textBoxVim.Text, textBoxEmployee.Text, textBoxLot.Text, 
+                textBoxItem.Text, textBoxQuantity.Text);
+
+        }
+
+
+        private void postJob(string vim, string employee, string lot, string item, string quantity)
+        {
+            using (WebClient client = new WebClient())
+            {
+
+                client.Headers.Add("Content-Type", "application/json");
+                try
+                {
+                    Job j = new Job();
+                    if (String.IsNullOrEmpty(vim))
+                    {
+                        j.Vim = 1;
+                    }
+                    else
+                    {
+                        j.Vim = Convert.ToInt32(vim);
+                    }
+                    if (String.IsNullOrEmpty(employee))
+                    {
+                        j.Employee = 1;
+                    }
+                    else
+                    {
+                        j.Employee = Convert.ToInt32(employee);
+                    }
+                    if (String.IsNullOrEmpty(lot))
+                    {
+                        j.Lot = 160118000601;
+                    }
+                    else
+                    {
+                        j.Lot = Convert.ToInt64(lot);
+                    }
+                    if (String.IsNullOrEmpty(item))
+                    {
+                        j.Item = "100-01";
+                    }
+                    else
+                    {
+                        j.Item = item;
+                    }
+                    if (String.IsNullOrEmpty(quantity))
+                    {
+                        j.Quantity = 137425;
+                    }
+                    else
+                    {
+                        j.Quantity = Convert.ToInt32(quantity);
+                    }
+                    string data = @"{" +
+                        @"""vim_id"":" + j.Vim + "," +
+                        @"""employee"":{""badge_number"":" + j.Employee + "}," +
+                        @"""lot"":{""designation"":" + j.Lot + "}," +
+                        @"""product"":{""designation"":""" + j.Item + @"""}," +
+                        @"""quantity"":" + j.Quantity + "}";
+                    Debug.Print(data);
+                    string response = client.UploadString("http://192.168.10.23:3000/jobs", "POST", data);
+                    Debug.Print(response);
+                }
+                catch (Exception ex)
+                {
+                    Debug.Print(ex.Message);
+                }
+            }
+        }
+
+    }
+
+
+    public class Job
+    {
+        private int _vim;
+        private int _employee;
+        private Int64 _lot;
+        private string _item;
+        private int _quantity;
+
+        public int Employee
+        {
+            get { return _employee; }
+            set { _employee = value; }
+        }
+
+        public int Vim
+        {
+            get { return _vim; }
+            set { _vim = value; }
+        }
+
+        public Int64 Lot
+        {
+            get { return _lot; }
+            set { _lot = value; }
+        }
+
+        public string Item
+        {
+            get { return _item; }
+            set { _item = value; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+    }
+
+}
